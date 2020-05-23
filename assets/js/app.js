6d45f1ceefd5ef0b813e5901c7776258edf1f0f3
Alexander_WP <alexandersp261@gmail.com>
2020-05-22 19:27:31 -0500

    Se comienza con testimoniales, pendiente slider, aun esta el video cargado - pendiente borrar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // funcion para elemento blinks en el titulo 
 function blinksNone() {
 	const blinks = document.querySelector('.toggle');
-	console.log();
 	blinks.classList.toggle("blinks")
 }
 setInterval(blinksNone, 500);
