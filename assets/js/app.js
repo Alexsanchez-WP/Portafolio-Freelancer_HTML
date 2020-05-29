ba591159f136b4b0f475c27c8fbc5790a4fed2b2
Alexander_WP <alexandersp261@gmail.com>
2020-05-28 19:08:45 -0500

    Se elimina jQueri, coverr y SliderBox... Se termina el maquetado de index y portafolio... se comienza con la pagina de perfil

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,26 +6,3 @@ function blinksNone() {
 setInterval(blinksNone, 1000);
 
 // funcion para los botones del slider
-
-function sliderHome() {
-	const slider1 = document.querySelector('#slider-1');
-	const slider2 = document.querySelector('#slider-2');
-	const slider3 = document.querySelector('#slider-3');
-	const btnSliders = document.querySelector('.slider-control').addEventListener('click', (event) => {
-		event.preventDefault();
-		console.log(event.target.innerText);
-		console.log(slider1);
-
-		if (event.target.innerText === '1') {
-			slider1.classList.add('slider-in');
-			slider1.classList.remove('slider-off');
-		} else if (event.target.innerText === '2') {
-			slider2.classList.add('slider-in');
-			slider2.classList.remove('slider-off');
-		} else if (event.target.innerText === '3') {
-			slider3.classList.add('slider-in');
-			slider3.classList.remove('slider-off');
-		}
-	});
-}
-sliderHome();
