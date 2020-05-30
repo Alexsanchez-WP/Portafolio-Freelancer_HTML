aefbc41819ad18cac074ee5cd1a2579dce396ebf
Alexander_WP <alexandersp261@gmail.com>
2020-05-29 19:37:56 -0500

    Se añaden los Skills a la pagina de perfil, pendite el ccs y javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,19 @@ function blinksNone() {
 	const blinks = document.querySelector('.toggle');
 	blinks.classList.toggle('blinks');
 }
-setInterval(blinksNone, 1000);
+setInterval(blinksNone, 2000);
 
-// funcion para los botones del slider
+// funcion para darle color a los iconos de los skills
+
+function skills() {
+	const doubleUp = document.querySelector('.content-profile');
+	doubleUp.addEventListener('click', event => {
+		event.preventDefault();
+		console.log(event.target.className)
+		if (event.target.className === "fas fa-angle-double-up fa-rotate-180") {
+
+		}
+
+	})
+}
+skills();
