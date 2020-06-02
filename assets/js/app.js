f653e4f1aff800af91ce32670375641f0b6ebaf5
Alexander_WP <alexandersp261@gmail.com>
2020-06-01 19:44:31 -0500

    Se hace animacion en perfil

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,15 @@ setInterval(blinksNone, 2000);
 // funcion para darle color a los iconos de los skills
 
 function skills() {
-	const doubleUp = document.querySelector('.content-profile');
+	const doubleUp = document.querySelector('.profile');
 	doubleUp.addEventListener('click', event => {
 		event.preventDefault();
-		console.log(event.target.className)
-		if (event.target.className === "fas fa-angle-double-up fa-rotate-180") {
-
+		if (event.target.classList.contains("skills") === true) {
+			let nodo = event.target.parentNode.nextElementSibling.classList;
+			console.log(nodo);
+			nodo.toggle('yes-meter');
+			nodo.toggle('no-meter');
 		}
-
 	})
 }
 skills();
